3495f59d6e9ae7849d649ea440dc4c331c03e52a
Oscar Sanhueza <oscar@hashdot.fi>
2022-01-15 20:28:54 +0200

    add seed for testing
    Signed-off-by: Oscar Sanhueza <oscar@hashdot.fi>

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+measurements = Measurement.create([
+  {"name":"Hemoglobiini2","unit":"g/l","upper_limit":"135","lower_limit":"168"},
+  {"name":"Hemoglobiini2","unit":"g/l","upper_limit":"135","lower_limit":"168"},
+  {"name":"LDL-kolesteroli","unit":"mmol/l","upper_limit":"0","lower_limit":"3"},
+  {"name":"LDL-kolesteroli2","unit":"mmol/l","upper_limit":"0","lower_limit":"3"},
+  ])
