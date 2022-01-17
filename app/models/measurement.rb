56f5b287e7d0df14e44ead06207f5991ef7076f7
Oscar Sanhueza <oscar@hashdot.fi>
2022-01-17 12:21:02 +0200

    lets use intergers here
    Came to my mind, maybe it would just been
    smarter to have numbers in databases, BUT,
    from my experience with currecies, is just
    better to convert
    
    Signed-off-by: Oscar Sanhueza <oscar@hashdot.fi>

--- app/models/measurement.rb
+++ app/models/measurement.rb
@@ -10,6 +10,6 @@ private
   def start_must_be_before_end_time
     return if not lower_limit or not upper_limit
     errors.add(:lower_limit, "lower must be lower") if
-       lower_limit > upper_limit
+       lower_limit.to_i > upper_limit.to_i
   end
 end
