ffdd9e264833454dcc733a39e61b5bd414f41ea7
Oscar Sanhueza <oscar@hashdot.fi>
2022-01-17 11:39:17 +0200

    test for name unit pressence
    Signed-off-by: Oscar Sanhueza <oscar@hashdot.fi>

--- test/models/measurement_test.rb
+++ test/models/measurement_test.rb
@@ -1,7 +1,28 @@
 require 'test_helper'
 
 class MeasurementTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @measurement = measurements(:one)
+  end
+
+  test "should create measurement" do
+    assert @measurement.name
+    assert @measurement.unit
+    assert @measurement.upper_limit
+    assert @measurement.lower_limit
+  end
+
+  test "name and units do not exist error" do
+    measurement = Measurement.new
+    assert_not measurement.save
+  end
+
+  test "name and units should exist" do
+    measurement = Measurement.new
+    measurement.name = "two"
+    measurement.unit = "m/ml"
+    measurement.lower_limit = "1"
+    measurement.upper_limit = "2.3"
+    assert measurement.save
+  end
 end
